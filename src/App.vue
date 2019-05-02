8f34b9390e36ded132ea8dcbf15202d2b5f9b4d3
Taisa Boiko <tayaboyko@gmail.com>
2019-05-02 21:42:43 +0300

    update(app): added naw drawer

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,72 @@
 <template>
   <v-app>
     <v-toolbar app>
+      <v-icon
+        @click.stop="drawer = !drawer">
+        menu
+      </v-icon>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span v-text="$t('appName')"></span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
         flat
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
+        target="_blank">
         <span class="mr-2">Latest Release</span>
       </v-btn>
     </v-toolbar>
 
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary>
+      <v-list class="pa-1">
+        <v-list-tile avatar>
+          <v-list-tile-avatar>
+            <img src="https://randomuser.me/api/portraits/women/10.jpg">
+          </v-list-tile-avatar>
+
+          <v-list-tile-content>
+            <v-list-tile-title>John Test</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+
+      <v-list class="pt-0" dense>
+        <v-divider></v-divider>
+
+        <v-list-tile
+          v-for="item in items"
+          :key="item.title">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-content>
-      <HelloWorld/>
+      <router-view/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
-  },
   data() {
     return {
-      //
+      drawer: null,
+      items: [
+        { title: 'Home', icon: 'dashboard' },
+        { title: 'About', icon: 'question_answer' },
+      ],
     };
   },
 };
