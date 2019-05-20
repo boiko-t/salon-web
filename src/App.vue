cd8d7f6c98f17e35a2229243321e15d9b37f6045
Taisa Boiko <tayaboyko@gmail.com>
2019-05-20 07:54:58 +0300

    add(Toolbar)

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,6 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-icon
-        @click.stop="drawer = !drawer">
-        menu
-      </v-icon>
-      <v-toolbar-title class="headline text-uppercase">
-        <span v-text="$t('appName')"></span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank">
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
-
-    <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      temporary>
-      <v-list class="pa-1">
-        <v-list-tile avatar>
-          <v-list-tile-avatar>
-            <img src="https://randomuser.me/api/portraits/women/10.jpg">
-          </v-list-tile-avatar>
-
-          <v-list-tile-content>
-            <v-list-tile-title>John Test</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-
-      <v-list class="pt-0" dense>
-        <v-divider></v-divider>
-
-        <v-list-tile
-          v-for="item in items"
-          :key="item.title">
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-
-          <v-list-tile-content>
-            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
+    <core-toolbar/>
 
     <v-content>
       <router-view/>
@@ -57,6 +9,7 @@
 </template>
 
 <script>
+import CoreToolbar from './components/core/Toolbar.vue';
 
 export default {
   name: 'App',
@@ -69,5 +22,12 @@ export default {
       ],
     };
   },
+  components: {
+    CoreToolbar,
+  },
 };
 </script>
+
+<style lang="scss">
+  @import 'assets/styles/imports';
+</style>
