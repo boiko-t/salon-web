81f7c96665bb18b93785168fb87ab5ae730a1256
Taisa.Boiko <Taisa.Boiko@infopulse.com>
2019-05-21 16:47:34 +0300

    Revert "add(Toolbar)"
    This reverts commit cd8d7f6c98f17e35a2229243321e15d9b37f6045.

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,10 +1,7 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
-import theme from './theme';
 import 'vuetify/src/stylus/app.styl';
-import '@mdi/font/css/materialdesignicons.css';
 
 Vue.use(Vuetify, {
-  iconfont: 'mdi',
-  theme,
+  iconfont: 'md',
 });
